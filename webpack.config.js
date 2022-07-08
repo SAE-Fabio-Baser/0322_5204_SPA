665803096651da1c60b6f103ba37d847e1ededdd
Fabio Baser <git@fabiobaser.de>
2022-07-08 15:59:38 +0200

    Replaced Parcel with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const config = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {},
+  stats: 'minimal',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+}
+
+module.exports = config
